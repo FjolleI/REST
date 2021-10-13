c4f56f2443203f481b8360619d24496acec7f077
Fjolle Islamaj <fjolle.islamaj@sqa-consulting.com>
2021-10-14 00:41:07 +0200

    config

--- src/main/java/com/coursestopics/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/coursestopics/springboot/config/SecurityConfiguration.java
@@ -4,8 +4,10 @@ import com.coursestopics.springboot.filter.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,13 +34,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+//            HttpSecurity http) throws Exception {
 //        http.csrf().disable().authorizeRequests().antMatchers("/authenticate",
 //                "/register")
 //                .permitAll().anyRequest().authenticated()
 //                .and().exceptionHandling().and().sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
-        http.authorizeRequests().antMatchers("*").permitAll();
+//        http.authorizeRequests().antMatchers("/**").permitAll();
     }
 }
